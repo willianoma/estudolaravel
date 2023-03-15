e093d4c1b902139252701f81d9ba8bc83ebab37d
willian <willianoma@hotmail.com>
2023-03-14 22:53:26 -0300

    configuracoes iniciais

---
+++ app/Http/Controllers/AgendaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\agenda;
+use Illuminate\Http\Request;
+
+class AgendaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\agenda  $agenda
+     * @return \Illuminate\Http\Response
+     */
+    public function show(agenda $agenda)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\agenda  $agenda
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(agenda $agenda)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\agenda  $agenda
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, agenda $agenda)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\agenda  $agenda
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(agenda $agenda)
+    {
+        //
+    }
+}
